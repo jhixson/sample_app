461f86a9b1a753396b081b0381b63b7fd3bce78d
Jesse Hixson <hixsonj@gmail.com>
2011-02-19 20:02:14 -0500

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
-  def title
-    base_title = "SampleApp"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
-=======
 	def title
 		base_title = "SampleApp"
 		if @title.nil?
@@ -17,5 +7,4 @@ module ApplicationHelper
 			"#{base_title} | #{@title}"
 		end
 	end
->>>>>>> f1c50c71ed273d2a23b64d9ed50b6ef84096f305
 end
